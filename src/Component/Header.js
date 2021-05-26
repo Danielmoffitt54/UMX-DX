0b715913e240e641be45a7c0a30b3e0b83390c99
Daniel Moffitt <Danielmoffitt54@gmail.com>
2021-05-26 16:10:37 -0500

    Finished styling header component.

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -4,133 +4,124 @@ import {
     Nav,
     NavDropdown,
     Container,
-    Row,
-    Col,
-    Table
+    Card,
+    ListGroup,
+    ListGroupItem
 } from 'react-bootstrap';
 import Events from '../Events';
 
 const Header = (props) => {
     
-    return <Navbar
-        style={{ flexFlow: 'row wrap'}}
-    >
+    return <Container fluid>
         {!props.Oauth2 ? (
-            <Container fluid>
-                <Row>
-                    <Col>
-                        <Navbar.Brand
-                            as={NavLink}
-                            to="/"
-                            onClick={() => props.SetAppState({
-                                Cerner: null,
-                                MainIndex: 0,
-                                AccountIndex: 0,
-                                DisplayCount: 25,
-                                DisplayIndex: 0
-                            })}
-                        >UMX-DX App</Navbar.Brand>
-                    </Col>
-                    <Col 
-                        as={NavDropdown}
-                        title="Login"
-                        id="navbarScrollingDropdown"
-                    >
-                        <NavDropdown.Item as={NavLink} to="/launch-patient">Patient</NavDropdown.Item>
-                        <NavDropdown.Item as={NavLink} to="/launch-provider">Provider</NavDropdown.Item>
-                    </Col>
-                </Row>
-            </Container>
+            <Navbar style={{ justifyContent: 'space-between' }}>
+                <Navbar.Brand
+                    as={NavLink}
+                    to="/"
+                    onClick={() => props.SetAppState({
+                        Cerner: null,
+                        MainIndex: 0,
+                        AccountIndex: 0,
+                        DisplayCount: 25,
+                        DisplayIndex: 0
+                    })}
+                >UMX-DX App</Navbar.Brand>
+                <NavDropdown
+                    title="Login"
+                    id="navbarScrollingDropdown"
+                >
+                    <NavDropdown.Item as={NavLink} to="/launch-patient">Patient</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/launch-provider">Provider</NavDropdown.Item>
+                </NavDropdown>
+            </Navbar>
         ) : (
-            <Container fluid>
-                <Row>
-                    <Col>
-                        <Navbar.Brand
+            <Navbar style={{ justifyContent: 'space-between' }}>
+                <Navbar.Brand
+                    as={NavLink}
+                    to="/"
+                    onClick={() => props.SetAppState({
+                        Cerner: null,
+                        MainIndex: 0,
+                        AccountIndex: 0,
+                        DisplayCount: 25,
+                        DisplayIndex: 0
+                    })}
+                >UMX-DX App</Navbar.Brand>
+                <NavDropdown
+                    title="Menu"
+                    id="navbarScrollingDropdown"
+                >
+                    {props.AllOptions.map((option, i) => {
+                        return <NavDropdown.Item 
+                            key={i}
                             as={NavLink}
-                            to="/"
-                            onClick={() => props.SetAppState({
-                                Cerner: null,
-                                MainIndex: 0,
-                                AccountIndex: 0,
-                                DisplayCount: 25,
-                                DisplayIndex: 0
-                            })}
-                        >UMX-DX App</Navbar.Brand>
-                    </Col>
-                    <Col
-                        as={NavDropdown}
-                        title="Menu"
-                        id="navbarScrollingDropdown"
-                    >
-                        {props.AllOptions.map((option, i) => {
-                            return <NavDropdown.Item 
-                                key={i}
-                                as={NavLink}
-                                to={option.path}
-                                onClick={() => {
-                                    props.SetAppState({ Loading: true });
-                                    Events.client.request({
-                                        Oauth2: props.Oauth2,
-                                        Page: option.value,
-                                        Property: `${props.PageType}Index`,
-                                        Value: i,
-                                        SetAppState: props.SetAppState
-                                    });
-                                }}
-                            >{option.label}</NavDropdown.Item>
+                            to={option.path}
+                            onClick={() => {
+                                props.SetAppState({ Loading: true });
+                                Events.client.request({
+                                    Oauth2: props.Oauth2,
+                                    Page: option.value,
+                                    Property: `${props.PageType}Index`,
+                                    Value: i,
+                                    SetAppState: props.SetAppState
+                                });
+                            }}
+                        >{option.label}</NavDropdown.Item>
+                    })}
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item
+                        as={NavLink}
+                        to="/" 
+                        onClick={() => props.SetAppState({
+                            Oauth2: null,
+                            Patient: null,
+                            Cerner: null,
+                            MainIndex: 0,
+                            AccountIndex: 0,
+                            DisplayCount: 25,
+                            DisplayIndex: 0
                         })}
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item
-                            as={NavLink}
-                            to="/" 
-                            onClick={() => props.SetAppState({
-                                Oauth2: null,
-                                Patient: null,
-                                Cerner: null,
-                                MainIndex: 0,
-                                AccountIndex: 0,
-                                DisplayCount: 25,
-                                DisplayIndex: 0
-                            })}
-                        >Log Out</NavDropdown.Item>
-                    </Col>
-                </Row>
-            </Container>
+                    >Log Out</NavDropdown.Item>
+                </NavDropdown>
+            </Navbar>
         )}
         {SubHeader(props)}
-    </Navbar>
+    </Container>
 };
 
 function SubHeader(props) {
 
     if (!props.Patient && !props.Cerner) return
+    if (props.Patient) console.log('Patient: ', props.Patient);
+
     return <Container 
-        style={{ 
-            flexWrap: 'wrap', 
-            justifyContent: 'center'
-        }}
+        noGutter
         fluid
     >
         {props.Patient ? (
-            <Row as={Table}>
-                <thead>
-                    {Events.thead.patient()}
-                </thead>
-                <tbody>
-                    {props.Patient.map((entry, index) => {
-                        return Events.tbody.patient({
-                            index: index,
-                            entry: entry
-                        });
-                    })}
-                </tbody>
-            </Row>
+            <Card style={{ margin: '0 0 5px 0' }}>
+                <Card.Body  style={{ 
+                    padding: '10px',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center' 
+                }}>
+                    <div>
+                        <Card.Title style={{ margin: '0' }}>{Events.value.officialName(props.Patient.name)}</Card.Title>
+                        <Card.Subtitle style={{ margin: '0' }}>{`ID: ${props.Patient.id}`}</Card.Subtitle>
+                    </div>
+                    <ListGroup 
+                        className="list-group-flush"
+                        style={{ flexDirection: "row" }}>
+                        <ListGroupItem style={{ border: 'none' }}>{`Gender: ${Events.value.capitalize(props.Patient.gender)}`}</ListGroupItem>
+                        <ListGroupItem>{`DoB: ${Events.value.date(props.Patient.birthDate)}`}</ListGroupItem>
+                    </ListGroup>
+                </Card.Body>
+            </Card>
         ) : null }
         {props.Cerner ? (
-            <Row as={Nav}>
-                <Col
-                    style={{ alignSelf: 'center'}}
-                >
+            <Nav style={{ justifyContent: "center", alignItems: "center" }}>
+                <Nav.Item>
                     <Nav.Link 
                         as={NavLink}
                         to="/" 
@@ -142,11 +133,9 @@ function SubHeader(props) {
                             DisplayIndex: 0
                         })}
                     >Back</Nav.Link>
-                </Col>
+                </Nav.Item>
                 {props.PageArray.map((option, i) => {
-                    if (props.PageIndex === i) return <Col
-                        style={{ alignSelf: 'center'}}
-                    >
+                    if (props.PageIndex === i) return <Nav.Item>
                         <Nav.Link key={i} 
                             as={NavLink}
                             to={option.path}
@@ -161,10 +150,8 @@ function SubHeader(props) {
                                 });
                             }}
                         >{option.label}</Nav.Link>
-                    </Col>
-                    return <Col
-                        style={{ alignSelf: 'center'}}
-                    >
+                    </Nav.Item>
+                    return <Nav.Item>
                         <Nav.Link key={i} 
                             as={NavLink}
                             to={option.path}
@@ -179,11 +166,9 @@ function SubHeader(props) {
                                 });
                             }}
                         >{option.label}</Nav.Link>
-                    </Col>
+                    </Nav.Item>
                 })}
-                <Col
-                    style={{ alignSelf: 'center'}}
-                >
+                <Nav.Item>
                     <Nav.Link
                         as={NavDropdown}
                         title={`Count: ${props.DisplayCount}`}
@@ -199,8 +184,8 @@ function SubHeader(props) {
                             onClick={() => props.SetAppState({ DisplayCount: 100, CurrentPage: 1, DisplayIndex: 0 })}
                         >100</NavDropdown.Item>
                     </Nav.Link>
-                </Col>
-            </Row>
+                </Nav.Item>
+            </Nav>
         ) : null }
     </Container>
 
